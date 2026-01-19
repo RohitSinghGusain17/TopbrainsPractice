078f5f4079a876adaf11f0c9de1457dc0ea88dee
RohitSinghGusain17 <rohitgusain672432@gmail.com>
2026-01-19 12:40:40 +0530

    Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    public static string GetHeightCategory(int heightCm)
+    {
+        if (heightCm < 150)
+            return "Short";
+        else if (heightCm < 180)
+            return "Average";
+        else
+            return "Tall";
+    }
+
+    public static void Main()
+    {
+        int heightCm = 170;
+        Console.WriteLine(GetHeightCategory(heightCm));
+    }
+}
