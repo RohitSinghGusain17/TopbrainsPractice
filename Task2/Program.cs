bcd3059156d31b7d6d309c4972cb8ce03476da88
RohitSinghGusain17 <rohitgusain672432@gmail.com>
2026-01-19 10:35:08 +0530

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n, upto;
+        n=int.Parse(Console.ReadLine()!);
+        upto=int.Parse(Console.ReadLine()!);
+
+        int[] result = new int[upto];//array of size upto to store result
+        for(int i = 1; i <= upto; i++)//loop for multiplication
+        {
+            result[i-1]=i*n;
+        }
+
+        for(int i = 0; i<upto; i++)//loop to print result
+        {
+            Console.Write(result[i]+" ");
+        }
+    }
+}
