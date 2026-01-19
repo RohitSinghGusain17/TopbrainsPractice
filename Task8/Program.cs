46f7f4fd19ab97c5cf8ef10b054961b7488ca0f8
RohitSinghGusain17 <rohitgusain672432@gmail.com>
2026-01-19 12:44:14 +0530

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    public static int GetFinalBalance(int initialBalance, int[] transactions)
+    {
+        int balance = initialBalance;
+
+        foreach (int transaction in transactions)
+        {
+            if (transaction >= 0)
+            {
+                balance += transaction;// Deposit
+            }
+            else
+            {
+                if (balance + transaction >= 0) // Withdraw only if enough balance
+                {
+                    balance += transaction;
+                }
+            }
+        }
+
+        return balance;
+    }
+
+    public static void Main()
+    {
+        int initialBalance = 1000;
+        int[] transactions = { 200, -300, -500, 400, -900 };
+
+        int finalBalance = GetFinalBalance(initialBalance, transactions);
+        Console.WriteLine(finalBalance);
+    }
+}
